0dbfcc8beeb935f289f3ad998b625eb0df042d48
jagrati7305 <jagrati07.work@gmail.com>
2025-12-22 14:07:58 +0530

    App dimension Added to make it Responsive

---
+++ lib/constants/dimension.dart
@@ -0,0 +1,10 @@
+import 'package:get/get.dart';
+
+class Dimension {
+static double screenHeight = Get.context!.height;
+static double screenWidth = Get.context!.width;
+
+static double heightFactor = screenHeight/874;
+static double widthFactor = screenHeight/402;
+
+}
