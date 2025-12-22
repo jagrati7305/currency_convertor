9966b0ebdf27b9c3d9edcb4e019b7dab2b0e94b7
jagrati7305 <jagrati07.work@gmail.com>
2025-12-22 14:07:30 +0530

    App Colors Initialised

---
+++ lib/constants/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/animation.dart';
+
+class AppColors{
+  static final Color backgroundColor=const Color(0xFFC7C5E5);
+  static final Color mainPurpleColor=const Color(0xFF131047);
+  static final Color textFieldColor=const Color(0xFF2C2966);
+  static final Color orangeColor=const Color(0xFFFFA051);
+}
