6c132f6eb3ce2f103f79863e9d91aecf6ba576d2
jagrati7305 <jagrati07.work@gmail.com>
2025-12-22 13:45:46 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Curvertor',
+      theme: ThemeData(
+        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: Scaffold(
+        appBar: AppBar(title: Text('Curvertor'),),
+        body: Center(
+          child: Text('My First App'),
+        ),
+      )
+    );
+  }
+}
+
