2e4d38ff27f1fcf16107b7d379aea67664a93403
jagrati7305 <jagrati07.work@gmail.com>
2025-12-25 23:45:22 +0530

    Model For Conversion Rates

---
+++ lib/models/currency_rate.dart
@@ -0,0 +1,26 @@
+class ExchangeRateModel {
+  final int amount;
+  final String base;
+  final String date;
+  final Map<String, double> rates;
+
+  ExchangeRateModel({
+    required this.amount,
+    required this.base,
+    required this.date,
+    required this.rates,
+  });
+
+  factory ExchangeRateModel.fromJson(Map<String, dynamic> json) {
+    return ExchangeRateModel(
+      amount: json["amount"],
+      base: json["base"],
+      date: json["date"],
+      rates: Map<String, double>.from(
+        json["rates"].map(
+          (key, value) => MapEntry(key, (value as num).toDouble()),
+        ),
+      ),
+    );
+  }
+}
