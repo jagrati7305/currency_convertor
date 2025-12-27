84d520898f3e5a1b1d8f763e3249fa1a42341d11
jagrati7305 <jagrati07.work@gmail.com>
2025-12-27 18:27:25 +0530

    Animation on the Splash Screen

--- lib/screen/splash_screen.dart
+++ lib/screen/splash_screen.dart
@@ -18,7 +18,7 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   late final Future<CurrencyNames> currencies;
   late final Future<ExchangeRateModel> rate;
-
+  double size=0;
   @override
   void initState() {
     super.initState();
@@ -26,7 +26,7 @@ class _SplashScreenState extends State<SplashScreen> {
     currencies = fetchCurrencies();
     rate = fetchRate();
 
-    Timer(Duration(seconds: 4), (){
+    Timer(Duration(seconds: 5), (){
         Navigator.pushReplacement(
           context,
           MaterialPageRoute(
@@ -40,77 +40,92 @@ class _SplashScreenState extends State<SplashScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.backgroundColor,
-      body: Column(
-            
-            children: [
-              SizedBox(
-                height: Dimension.screenHeight/2.5,
-              ),
-              Container(
-                margin: EdgeInsets.only(bottom: Dimension.heightFactor*4),
-                width: Dimension.widthFactor*100,
-                height: Dimension.widthFactor*100,
-                decoration: BoxDecoration(
-                  color: AppColors.mainPurpleColor,
-                  shape: BoxShape.circle
-                ),
-                child: Center(
-                  child: Icon(
-                    Icons.currency_exchange,
-                    color: AppColors.textFieldColor,
-                    size: Dimension.widthFactor*45,
-                  ),
-                ),
-              ),
-              Text(
-                'CURVERTOR',
-                style: TextStyle(
-                  color: AppColors.textFieldColor,
-                  fontSize: Dimension.heightFactor*24,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 1.5
-                ),
-              ),
-              SizedBox(
-                height: Dimension.screenHeight/2.8,
-              ),
-              Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
+      body: TweenAnimationBuilder(
+        tween: Tween(begin: 0.0,end: 100.0),
+        duration: Duration(milliseconds: 2000),
+        builder: (context,size,widget) {
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          'Made with',
-                          style: TextStyle(
-                            color: AppColors.textFieldColor,
-                            fontWeight: FontWeight.bold,
-                            fontSize: Dimension.heightFactor*14
-                          ),
-                        ),
-                        SizedBox(
-                          width: Dimension.widthFactor*4,
-                        ),
-                        Icon(
-                          Icons.favorite,
+                    SizedBox(
+                      height: Dimension.screenHeight/2.5,
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(bottom: Dimension.heightFactor*4),
+                      width: size,
+                      height: size,
+                      decoration: BoxDecoration(
+                        color: AppColors.mainPurpleColor,
+                        shape: BoxShape.circle,
+                        boxShadow:[
+                          BoxShadow(
+                            color: Color.fromRGBO(3, 81, 182, 0.2),
+                            blurRadius: size,
+                            spreadRadius: size/2
+                          )
+                        ]
+                      ),
+                      child: Center(
+                        child: Icon(
+                          Icons.currency_exchange,
                           color: AppColors.textFieldColor,
-                          size: Dimension.heightFactor*14,
-                        )
-                      ],
+                          size: size/2,
+                        ),
+                      ),
                     ),
                     Text(
-                      'Jagrati',
+                      'CURVERTOR',
                       style: TextStyle(
-                        fontSize: Dimension.heightFactor*16,
                         color: AppColors.textFieldColor,
-                        fontWeight: FontWeight.bold
+                        fontSize: size/4,
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: 1.5
                       ),
-                    )
-                  ],
-                ),
-            ],
-          
-        ),
+                    ),
+                    SizedBox(
+                      height: Dimension.screenHeight/2.8,
+                    ),
+                    Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                'Made with',
+                                style: TextStyle(
+                                  color: AppColors.textFieldColor,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: Dimension.heightFactor*14
+                                ),
+                              ),
+                              SizedBox(
+                                width: Dimension.widthFactor*4,
+                              ),
+                              Icon(
+                                Icons.favorite,
+                                color: AppColors.textFieldColor,
+                                size: size/7,
+                              )
+                            ],
+                          ),
+                          Text(
+                            'Jagrati',
+                            style: TextStyle(
+                              fontSize: size/7,
+                              color: AppColors.textFieldColor,
+                              fontWeight: FontWeight.bold
+                            ),
+                          )
+                        ],
+                      ),
+                      
+                  ],      
+              ),
+          );
+        }
+      ),
       
     );
   }
