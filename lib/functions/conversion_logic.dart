85a50f1d0a6c1b2bf86ca227dea78c5a0f4f0cb4
jagrati7305 <jagrati07.work@gmail.com>
2025-12-27 13:56:50 +0530

    Final Conversion Logic

--- lib/functions/conversion_logic.dart
+++ lib/functions/conversion_logic.dart
@@ -1,16 +1,28 @@
 
 import 'package:currency_convertor/models/currency_rate.dart';
 
+
 String conversion(ExchangeRateModel rateExchange, String baseCountry ,String toCountry ,String amount){
+  final double? baseRate;
+  if(rateExchange.rates[baseCountry]==null){
+      baseRate=1;
+  }else{
+    baseRate = rateExchange.rates[baseCountry];
+  }
+
+  final double? toRate;
   
-  final double? baseRate = rateExchange.rates[baseCountry];
-  final double? toRate = rateExchange.rates[toCountry];
+  if(rateExchange.rates[toCountry]==null){
+      toRate=1;
+  }else{
+      toRate = rateExchange.rates[toCountry];
+  }
 
   if (baseRate == null || toRate == null) {
+
     return "0.0000";
   }
 
   final double result = (double.parse(amount)/baseRate)*toRate;
-  print(result.toString());
-  return result.toStringAsFixed(4);
+  return result.toStringAsFixed(2);
 }
