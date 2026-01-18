2364004463a722347ef14e55050dda8362efd4a6
jagrati7305 <jagrati07.work@gmail.com>
2026-01-18 11:52:07 +0530

    ui : refine splash screen UI

--- lib/screen/splash_screen.dart
+++ lib/screen/splash_screen.dart
@@ -18,116 +18,113 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   late final Future<CurrencyNames> currencies;
   late final Future<ExchangeRateModel> rate;
-  double size=0;
+  double size = 0;
   @override
   void initState() {
     super.initState();
-      
+
     currencies = fetchCurrencies();
     rate = fetchRate();
 
-    Timer(Duration(seconds: 5), (){
-        Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(
-            builder: (context)=> CurvertorMainPage(currenciesFuture:  currencies,rateFuture: rate,),
-          )
-        );
+    Timer(Duration(seconds: 5), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) =>
+              CurvertorMainPage(currenciesFuture: currencies, rateFuture: rate),
+        ),
+      );
     });
-  
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.backgroundColor,
       body: TweenAnimationBuilder(
-        tween: Tween(begin: 0.0,end: 100.0),
+        tween: Tween(begin: 0.0, end: 100.0),
         duration: Duration(milliseconds: 2000),
-        builder: (context,size,widget) {
+        builder: (context, size, widget) {
           return Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    SizedBox(
-                      height: Dimension.screenHeight/2.5,
-                    ),
-                    Container(
-                      margin: EdgeInsets.only(bottom: Dimension.heightFactor*4),
-                      width: size,
-                      height: size,
-                      decoration: BoxDecoration(
-                        color: AppColors.mainPurpleColor,
-                        shape: BoxShape.circle,
-                        boxShadow:[
-                          BoxShadow(
-                            color: Color.fromRGBO(3, 81, 182, 0.2),
-                            blurRadius: size,
-                            spreadRadius: size/2
-                          )
-                        ]
+              children: [
+                SizedBox(height: Dimension.screenHeight / 2.5),
+                Container(
+                  margin: EdgeInsets.only(bottom: Dimension.heightFactor * 4),
+                  width: size,
+                  height: size,
+                  decoration: BoxDecoration(
+                    color: AppColors.mainPurpleColor,
+                    shape: BoxShape.circle,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Color.fromRGBO(3, 81, 182, 0.2),
+                        blurRadius: size,
+                        spreadRadius: size / 2,
                       ),
-                      child: Center(
-                        child: Image.asset(
-                          'assets/app_logo.png',
-                            width: size/1.75,
-                            height: size/1.75,
-                            fit: BoxFit.cover,
-                        )
-                      ),
+                    ],
+                  ),
+                  child: Center(
+                    child: Image.asset(
+                      'assets/app_logo.png',
+                      width: size / 1.75,
+                      height: size / 1.75,
+                      fit: BoxFit.cover,
                     ),
-                    Text(
-                      'CURVERTOR',
-                      style: TextStyle(
-                        color: AppColors.textFieldColor,
-                        fontSize: size/4,
-                        fontWeight: FontWeight.bold,
-                        letterSpacing: 1.5
-                      ),
-                    ),
-                    SizedBox(
-                      height: Dimension.screenHeight/2.8,
-                    ),
-                    Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text(
-                                'Made with',
-                                style: TextStyle(
-                                  color: AppColors.textFieldColor,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: Dimension.heightFactor*14
-                                ),
-                              ),
-                              SizedBox(
-                                width: Dimension.widthFactor*4,
-                              ),
-                              Icon(
-                                Icons.favorite,
-                                color: AppColors.textFieldColor,
-                                size: size/7,
-                              )
-                            ],
-                          ),
-                          Text(
-                            'Jagrati',
-                            style: TextStyle(
-                              fontSize: size/7,
-                              color: AppColors.textFieldColor,
-                              fontWeight: FontWeight.bold
-                            ),
-                          )
-                        ],
-                      ),
-                      
-                  ],      
-              ),
+                  ),
+                ),
+                Text(
+                  'CURVERTOR',
+                  style: TextStyle(
+                    color: AppColors.textFieldColor,
+                    fontSize: size / 4,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 1.5,
+                  ),
+                ),
+                SizedBox(height: Dimension.screenHeight / 2.8),
+              ],
+            ),
           );
-        }
+        },
       ),
-      
+      bottomNavigationBar: Container(
+        height: Dimension.heightFactor * 60,
+        color: AppColors.backgroundColor,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Made with',
+                  style: TextStyle(
+                    color: AppColors.textFieldColor,
+                    fontWeight: FontWeight.bold,
+                    fontSize: Dimension.heightFactor * 14,
+                  ),
+                ),
+                SizedBox(width: Dimension.widthFactor * 4),
+                Icon(
+                  Icons.favorite,
+                  color: AppColors.textFieldColor,
+                  size: 14,
+                ),
+              ],
+            ),
+            Text(
+              'Jagrati',
+              style: TextStyle(
+                fontSize: 14,
+                color: AppColors.textFieldColor,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
